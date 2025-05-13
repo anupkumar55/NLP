c836efafaf240381210bcaa2ea6a5f8265f56c62
Anup Kumar <anupsnap@gmail.com>
2025-05-13 16:33:43 +0530

    Update for port deployment in backend

--- backend/app.py
+++ backend/app.py
@@ -88,4 +88,7 @@ async def recognize_and_translate(file: UploadFile = File(...), dest_lang: str =
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
+
